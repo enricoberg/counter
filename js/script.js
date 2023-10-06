eca875a247aee06574f4d9867dd5ef7219db1901
unknown <enricobergamini@outlook.it>
2023-10-06 12:37:59 +0200

    Added decimal buttons

--- js/script.js
+++ js/script.js
@@ -1,8 +1,32 @@
-const box=document.querySelector("#valuebox");
-document.querySelector("#button-plus").addEventListener("click",()=>{
-    box.value=Number(box.value)+1;
-});
-document.querySelector("#button-minus").addEventListener("click",()=>{
-    box.value=Number(box.value)-1;
+const box = document.querySelector("#valuebox");
+const decimalbox = document.querySelector("#decimalbox");
+let currentNumber = 0;
+
+document.querySelector("#button-plus").addEventListener("click", () => {
+  currentNumber = currentNumber + 1;
+  displayNumber(currentNumber);
 });
 
+document.querySelector("#button-minus").addEventListener("click", () => {
+  currentNumber = currentNumber - 1;
+  displayNumber(currentNumber);
+});
+
+document.querySelector("#decimal-plus").addEventListener("click", () => {
+  currentNumber = (currentNumber  + 0.01);
+  displayNumber(currentNumber);
+});
+
+document.querySelector("#decimal-minus").addEventListener("click", () => {
+  currentNumber = (currentNumber  - 0.01) ;
+  displayNumber(currentNumber);
+});
+
+function displayNumber(number) {
+  let decimals = Math.abs((number % 1) * 100);
+  let integerpart = number>0 ? Math.floor(number) : Math.ceil(number);
+  integerpart= integerpart==100 ? 0 : integerpart;
+  console.log(number);
+  box.value = integerpart;
+  decimalbox.value = (number < 0 ? "-" : "") + String(Math.abs(decimals).toFixed(0)).padStart(2, "0");
+}
