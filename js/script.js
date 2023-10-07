39e07deacd96a2bbe1d32d059b47207b4f0a3436
unknown <enricobergamini@outlook.it>
2023-10-07 18:48:30 +0200

    Added events for handling prolonged click of the buttons

--- js/script.js
+++ js/script.js
@@ -1,26 +1,12 @@
 const box = document.querySelector("#valuebox");
 const decimalbox = document.querySelector("#decimalbox");
+const add100=document.querySelector("#button-plus");
+const add1=document.querySelector("#decimal-plus");
+const sub100=document.querySelector("#button-minus");
+const sub1=document.querySelector("#decimal-minus");
+let numbertimer;
+const timerspeed=100;
 let currentNumber = 0;
-
-document.querySelector("#button-plus").addEventListener("click", () => {
-  currentNumber = currentNumber + 1;
-  displayNumber(currentNumber);
-});
-
-document.querySelector("#button-minus").addEventListener("click", () => {
-  currentNumber = currentNumber - 1;
-  displayNumber(currentNumber);
-});
-
-document.querySelector("#decimal-plus").addEventListener("click", () => {
-  currentNumber = (currentNumber  + 0.01);
-  displayNumber(currentNumber);
-});
-
-document.querySelector("#decimal-minus").addEventListener("click", () => {
-  currentNumber = (currentNumber  - 0.01) ;
-  displayNumber(currentNumber);
-});
 
 function displayNumber(number) {
   let decimals = Math.abs((number % 1) * 100);
@@ -30,3 +16,88 @@ function displayNumber(number) {
   box.value = (number<0 && number>-1)? "-0" : integerpart;
   decimalbox.value =  String(Math.abs(decimals).toFixed(0)).padStart(2, "0");
 }
+//----------------------------------------------------
+// EVENTS FOR HANDLING SINGLE CLICKS OF THE BUTTON----
+
+add100.addEventListener("click", () => {
+  currentNumber = currentNumber + 1;
+  displayNumber(currentNumber);
+});
+
+sub100.addEventListener("click", () => {
+  currentNumber = currentNumber - 1;
+  displayNumber(currentNumber);
+});
+
+add1.addEventListener("click", () => {
+  currentNumber = (currentNumber  + 0.01);
+  displayNumber(currentNumber);
+});
+
+sub1.addEventListener("click", () => {
+  currentNumber = (currentNumber  - 0.01) ;
+  displayNumber(currentNumber);
+});
+
+//----------------------------------------------------
+//EVENTS FOR HANDLING PROLONGED CLICKS----------------
+
+add100.addEventListener("mousedown",()=>{
+  
+  numbertimer=setInterval(()=>{
+    currentNumber = (currentNumber  + 1);
+    displayNumber(currentNumber);
+  },timerspeed);
+
+
+});
+
+add100.addEventListener("mouseup",()=>{
+clearInterval(numbertimer);
+numbertimer=null;
+});
+
+add1.addEventListener("mousedown",()=>{
+  
+  numbertimer=setInterval(()=>{
+    currentNumber = (currentNumber  + 0.01);
+    displayNumber(currentNumber);
+  },timerspeed);
+
+
+});
+
+add1.addEventListener("mouseup",()=>{
+clearInterval(numbertimer);
+numbertimer=null;
+});
+
+sub100.addEventListener("mousedown",()=>{
+  
+  numbertimer=setInterval(()=>{
+    currentNumber = (currentNumber  - 1);
+    displayNumber(currentNumber);
+  },timerspeed);
+
+
+});
+
+sub100.addEventListener("mouseup",()=>{
+clearInterval(numbertimer);
+numbertimer=null;
+});
+
+sub1.addEventListener("mousedown",()=>{
+  
+  numbertimer=setInterval(()=>{
+    currentNumber = (currentNumber  - 0.01);
+    displayNumber(currentNumber);
+  },timerspeed);
+
+
+});
+
+sub1.addEventListener("mouseup",()=>{
+clearInterval(numbertimer);
+numbertimer=null;
+});
