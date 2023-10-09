db3fc78faad31a72bd9a640bbc0278e552d0bdab
unknown <enricobergamini@outlook.it>
2023-10-09 10:45:47 +0200

    Simplified button click events

--- js/counter.js
+++ js/counter.js
@@ -4,6 +4,8 @@ const add100=document.querySelector("#button-plus");
 const add1=document.querySelector("#decimal-plus");
 const sub100=document.querySelector("#button-minus");
 const sub1=document.querySelector("#decimal-minus");
+const counterbuttons=[add100,add1,sub100,sub1];
+const increments=[1,0.01,-1,-0.01];
 let numbertimer;
 const timerspeed=100;
 let currentNumber = 0;
@@ -16,31 +18,10 @@ function displayNumber(number) {
   box.value = (number<0 && number>-1)? "-0" : integerpart;
   decimalbox.value =  String(Math.abs(decimals).toFixed(0)).padStart(2, "0");
 }
-//----------------------------------------------------
-// EVENTS FOR HANDLING SINGLE CLICKS OF THE BUTTON----
 
-add100.addEventListener("click", () => {
-  currentNumber = currentNumber + 1;
-  displayNumber(currentNumber);
-});
-
-sub100.addEventListener("click", () => {
-  currentNumber = currentNumber - 1;
-  displayNumber(currentNumber);
-});
-
-add1.addEventListener("click", () => {
-  currentNumber = (currentNumber  + 0.01);
-  displayNumber(currentNumber);
-});
-
-sub1.addEventListener("click", () => {
-  currentNumber = (currentNumber  - 0.01) ;
-  displayNumber(currentNumber);
-});
 
 //----------------------------------------------------
-//EVENTS FOR HANDLING PROLONGED CLICKS----------------
+//FUNCTIONS TO HANDLE PROLONGED CLICKS----------------
 function startTimer(increment){
   
     numbertimer=setInterval(()=>{
@@ -53,72 +34,34 @@ function stopTimer(){
   clearInterval(numbertimer);
   numbertimer=null;
 }
-add100.addEventListener("mousedown", () => {
-  startTimer(1);
-});
 
-add100.addEventListener("mouseup", () => {
-  stopTimer();
-});
 
-add1.addEventListener("mousedown", () => {
-  startTimer(0.01);
-});
+//-------------------------------------------------------
+// EVENTS FOR HANDLING SINGLE CLICKS AND PROLONGED CLICKS
+for (let i=0; i<counterbuttons.length; i++) {
+  counterbuttons[i].addEventListener("click", () => {
+    currentNumber = currentNumber + increments[i];
+    displayNumber(currentNumber);
+  });
+  
+  counterbuttons[i].addEventListener("mousedown", () => {
+    
+    startTimer(increments[i]);
+  });
+  
+  counterbuttons[i].addEventListener("mouseup", () => {
+    
+    stopTimer();
+  });
 
-add1.addEventListener("mouseup", () => {
-  stopTimer();
-});
+  counterbuttons[i].addEventListener("touchstart", (e) => {
+    e.preventDefault();
+    startTimer(increments[i]);
+  });
+  
+  counterbuttons[i].addEventListener("touchend", () => {
+    
+    stopTimer();
+  });
+}
 
-sub100.addEventListener("mousedown", () => {
-  startTimer(-1);
-});
-
-sub100.addEventListener("mouseup", () => {
-  stopTimer();
-});
-
-sub1.addEventListener("mousedown", () => {
-  startTimer(-0.01);
-});
-
-sub1.addEventListener("mouseup", () => {
-  stopTimer();
-});
-//----------------------------------------------------
-//EVENTS FOR MOBILE USE-------------------------------
-
-add100.addEventListener("touchstart", (event) => {
-  event.preventDefault();
-  startTimer(1);
-});
-
-add100.addEventListener("touchend", () => {
-  stopTimer();
-});
-
-add1.addEventListener("touchstart", (event) => {
-  event.preventDefault();
-  startTimer(0.01);
-});
-
-add1.addEventListener("touchend", () => {
-  stopTimer();
-});
-
-sub100.addEventListener("touchstart", (event) => {
-  event.preventDefault();
-  startTimer(-1);
-});
-
-sub100.addEventListener("touchend", () => {
-  stopTimer();
-});
-
-sub1.addEventListener("touchstart", (event) => {
-  event.preventDefault();
-  startTimer(-0.01);
-});
-
-sub1.addEventListener("touchend", () => {
-  stopTimer();
-});
